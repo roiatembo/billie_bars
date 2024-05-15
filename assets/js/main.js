b46a4fb125e9a45a9d1f76b2e4601f6b949286a5
Roia Tembo <roia@itmogul.itmogul.co.za>
2024-05-15 20:25:03 +0200

    Three column slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,30 @@ var heroSplide = new Splide(".splide-hero", {
   perPage: 1,
   perMove: 1,
   pagination: true,
-  focus: 0
+  focus: 0,
 });
 
 heroSplide.mount();
+
+var carouselSplide = new Splide(".splide-carousel", {
+  // type: "loop",
+  perPage: 3,
+  perMove: 5,
+  pagination: false,
+  focus: 0,
+  arrows: false,
+  // autoplay: true,
+  // speed: 150000,
+  // interval: 0
+});
+
+carouselSplide.mount();
+
+var threeColumnSplide = new Splide(".splide-three-column", {
+  type: "loop",
+  perPage: 3,
+  perMove: 1,
+  gap: 10,
+});
+
+threeColumnSplide.mount();
